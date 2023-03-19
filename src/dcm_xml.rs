2e81cf780921e2913f9b18dd6b44af35a7714045
b123400 <i@b123400.net>
2023-03-19 11:42:05 +0900

    Take optional external xml as base

--- src/dcm_xml.rs
+++ src/dcm_xml.rs
@@ -1,18 +1,187 @@
-use crate::gdt::{ GdtBasicDiagnosticsObject, GdtFile, GdtPatientGender, GdtPatientObject };
+use std::fs::File;
+use std::path::Path;
 use std::str::FromStr;
 
-pub fn file_to_xml(file: GdtFile) -> String {
-    return format!(
-        r#"<?xml version="1.0" encoding="UTF-8"?>
+use tempfile::NamedTempFile;
+use xml::attribute::OwnedAttribute;
+use xml::name::OwnedName;
+use xml::reader::EventReader;
+use xml::writer::EventWriter;
+use xml::reader::XmlEvent;
+
+use crate::gdt::{ GdtBasicDiagnosticsObject, GdtFile, GdtPatientGender, GdtPatientObject };
+
+#[derive(Debug)]
+pub enum DcmError {
+    IoError(std::io::Error),
+    XmlReaderError(xml::reader::Error),
+    XmlWriterError(xml::writer::Error),
+}
+
+pub fn parse_dcm_xml(path: &Path) -> Result<Vec<XmlEvent>, DcmError> {
+    let file = File::open(path).map_err(DcmError::IoError)?;
+    let reader = EventReader::new(file);
+    let mut events: Vec<XmlEvent> = reader.into_iter().collect::<Result<Vec<_>,xml::reader::Error>>().map_err(DcmError::XmlReaderError)?;
+    add_meta_header_if_not_exist(&mut events);
+    return Ok(events);
+}
+
+pub fn xml_events_to_file(events: Vec<XmlEvent>) -> Result<NamedTempFile, DcmError> {
+    let temp_file = NamedTempFile::new().map_err(DcmError::IoError)?;
+    // std::fs::write(&temp_file, xml)?;
+    let mut writer = EventWriter::new(&temp_file);
+    for e in events {
+        match e.as_writer_event() {
+            Some(e)=> writer.write(e).map_err(DcmError::XmlWriterError)?,
+            _=> (), // events like EndDocument are ignored
+        };
+    };
+    return Ok(temp_file);
+}
+
+fn xml_contains(xml_events: &Vec<XmlEvent>, tag: String, name: String)-> bool {
+    xml_events.iter().any(|ev| {
+        match ev {
+            XmlEvent::StartElement { name: xml::name::OwnedName { local_name, .. }, attributes, .. }
+                if local_name.as_str() == "element" && attributes_contain(attributes, tag.clone(), name.clone()) => {
+                    true
+                }
+            _ => false,
+        }
+    })
+}
+
+fn value_of_attribute(attrs: &Vec<OwnedAttribute>, name: String)-> Option<String> {
+    attrs.iter().find_map(|OwnedAttribute { name: xml::name::OwnedName { local_name, .. }, value }| {
+        if *local_name == name {
+            Some(value.clone())
+        } else {
+            None
+        }
+    })
+}
+
+pub struct DcmElement {
+    pub tag: String,
+    pub vr: String,
+    pub name: String,
+    pub body: String,
+}
+
+fn add_element_if_not_exist(events: &mut Vec<XmlEvent>, element: DcmElement) {
+    if xml_contains(events, element.tag.clone(), element.name.clone()) {
+        return;
+    }
+    let end_data_set_index = events.iter().position(|e| match e {
+        XmlEvent::EndElement { name: OwnedName { local_name, .. } } if local_name.as_str() == "data-set" => true,
+        _ => false,
+    }).unwrap_or(events.len() - 2);
+    let start = XmlEvent::StartElement {
+        name: OwnedName {
+            local_name: "element".to_string(),
+            namespace: None,
+            prefix: None,
+        },
+        attributes: vec![
+            OwnedAttribute {
+                name: OwnedName {
+                    local_name: "tag".to_string(),
+                    namespace: None,
+                    prefix: None,
+                },
+                value: element.tag,
+            },
+            OwnedAttribute {
+                name: OwnedName {
+                    local_name: "vr".to_string(),
+                    namespace: None,
+                    prefix: None,
+                },
+                value: element.vr,
+            },
+            OwnedAttribute {
+                name: OwnedName {
+                    local_name: "vm".to_string(),
+                    namespace: None,
+                    prefix: None,
+                },
+                value: "1".to_string(),
+            },
+            OwnedAttribute {
+                name: OwnedName {
+                    local_name: "len".to_string(),
+                    namespace: None,
+                    prefix: None,
+                },
+                value: "0".to_string(), // TODO
+            },
+            OwnedAttribute {
+                name: OwnedName {
+                    local_name: "name".to_string(),
+                    namespace: None,
+                    prefix: None,
+                },
+                value: element.name,
+            },
+        ],
+        namespace: xml::namespace::Namespace::empty(),
+    };
+
+    let body = XmlEvent::Characters(element.body);
+    let end = XmlEvent::EndElement {
+        name: OwnedName {
+            local_name: "element".to_string(),
+            namespace: None,
+            prefix: None,
+        },
+    };
+    events.insert(end_data_set_index, start);
+    events.insert(end_data_set_index + 1, body);
+    events.insert(end_data_set_index + 2, end);
+}
+
+fn attributes_contain(attrs: &Vec<OwnedAttribute>, tag: String, name: String)-> bool {
+    let xml_tag = value_of_attribute(attrs, "tag".to_string());
+    let xml_name = value_of_attribute(attrs, "name".to_string());
+    return xml_tag == Some(tag) && xml_name == Some(name);
+}
+
+fn add_meta_header_if_not_exist(events: &mut Vec<XmlEvent>) {
+    let has_meta_header = events.iter().any(|e| match e {
+        XmlEvent::StartElement { name: OwnedName { local_name , .. }, .. } if local_name.as_str() == "meta-header" => true,
+        _ => false,
+    });
+    if has_meta_header {
+        return;
+    }
+    let open_data_set_index = events.iter().position(|e| match e {
+        XmlEvent::StartElement { name: OwnedName { local_name, ..}, ..} if local_name.as_str() == "data-set" => true,
+        _ => false,
+    }).unwrap_or(2);
+    let xml = r#"
+    <meta-header xfer="1.2.840.10008.1.2.1" name="Little Endian Explicit">
+    <element tag="0002,0000" vr="UL" vm="1" len="4" name="FileMetaInformationGroupLength">0</element>
+    <element tag="0002,0002" vr="UI" vm="1" len="28" name="MediaStorageSOPClassUID">1.2.840.10008.5.1.4.1.1.7.2</element>
+    <element tag="0002,0003" vr="UI" vm="1" len="50" name="MediaStorageSOPInstanceUID"></element>
+    <element tag="0002,0010" vr="UI" vm="1" len="22" name="TransferSyntaxUID"></element>
+    <element tag="0002,0012" vr="UI" vm="1" len="28" name="ImplementationClassUID"></element>
+    <element tag="0002,0013" vr="SH" vm="1" len="16" name="ImplementationVersionName"></element>
+    </meta-header>"#;
+    let reader = EventReader::new(xml.as_bytes());
+    let new_events: Vec<XmlEvent> = reader
+        .into_iter()
+        .filter(|e| match e {
+            Ok(XmlEvent::StartDocument {..}) | Ok(XmlEvent::EndDocument) => false,
+            _ => true,
+        })
+        .collect::<Result<Vec<_>,xml::reader::Error>>()
+        .unwrap();
+    events.splice((open_data_set_index - 1)..(open_data_set_index - 1), new_events);
+}
+
+pub fn default_dcm_xml()-> Vec<XmlEvent> {
+    let xml = r#"<?xml version="1.0" encoding="UTF-8"?>
 <file-format>
-<meta-header xfer="1.2.840.10008.1.2.1" name="Little Endian Explicit">
-<element tag="0002,0000" vr="UL" vm="1" len="4" name="FileMetaInformationGroupLength">0</element>
-<element tag="0002,0002" vr="UI" vm="1" len="28" name="MediaStorageSOPClassUID">1.2.840.10008.5.1.4.1.1.7.2</element>
-<element tag="0002,0003" vr="UI" vm="1" len="50" name="MediaStorageSOPInstanceUID"></element>
-<element tag="0002,0010" vr="UI" vm="1" len="22" name="TransferSyntaxUID"></element>
-<element tag="0002,0012" vr="UI" vm="1" len="28" name="ImplementationClassUID"></element>
-<element tag="0002,0013" vr="SH" vm="1" len="16" name="ImplementationVersionName"></element>
-</meta-header>
 <data-set xfer="1.2.840.10008.1.2.4.50" name="JPEG Baseline">
 <element tag="0008,0016" vr="UI" vm="1" len="28" name="SOPClassUID">1.2.840.10008.5.1.4.1.1.7.2</element>
 <element tag="0008,0018" vr="UI" vm="1" len="50" name="SOPInstanceUID"></element>
@@ -20,25 +189,65 @@ pub fn file_to_xml(file: GdtFile) -> String {
 <element tag="0008,0030" vr="TM" vm="0" len="0" name="StudyTime"></element>
 <element tag="0008,0050" vr="SH" vm="0" len="0" name="AccessionNumber"></element>
 <element tag="0008,0090" vr="PN" vm="0" len="0" name="ReferringPhysicianName"></element>
-<element tag="0010,0010" vr="PN" vm="0" len="0" name="PatientName">{patient_name}</element>
-<element tag="0010,0020" vr="LO" vm="0" len="0" name="PatientID">{patient_id}</element>
-<element tag="0010,0030" vr="DA" vm="0" len="0" name="PatientBirthDate">{patient_dob}</element>
-<element tag="0010,0040" vr="CS" vm="0" len="0" name="PatientSex">{patient_sex}</element>
-<element tag="0010,1020" vr="DS" vm="0" len="0" name="PatientSize">{patient_size}</element>
-<element tag="0010,1030" vr="DS" vm="0" len="0" name="PatientWeight">{patient_weight}</element>
 <element tag="0020,0010" vr="SH" vm="0" len="0" name="StudyID"></element>
 <element tag="0020,0011" vr="IS" vm="0" len="0" name="SeriesNumber"></element>
 <element tag="0020,0013" vr="IS" vm="0" len="0" name="InstanceNumber"></element>
 </data-set>
 </file-format>
-    "#,
-    patient_name=gdt_get_patient_name(&file.object_patient),
-    patient_id=file.object_patient.patient_number,
-    patient_dob=gdt_date_to_dcm(file.object_patient.patient_dob),
-    patient_sex=gdt_gender_to_dcm(&file.object_patient.patient_gender),
-    patient_size=gdt_get_patient_height_in_meters(&file.object_basic_diagnostics).unwrap_or("".to_string()),
-    patient_weight=file.object_basic_diagnostics.patient_weight,
-    )
+    "#;
+    let reader = EventReader::new(xml.as_bytes());
+    let mut events: Vec<XmlEvent> = reader
+        .into_iter()
+        .filter(|e| match e {
+            Ok(XmlEvent::StartDocument {..}) | Ok(XmlEvent::EndDocument) => false,
+            _ => true,
+        })
+        .collect::<Result<Vec<_>,xml::reader::Error>>()
+        .unwrap();
+    add_meta_header_if_not_exist(&mut events);
+    return events;
+}
+
+pub fn file_to_xml(file: GdtFile, xml_events: &Vec<XmlEvent>) ->  Result<NamedTempFile, DcmError> {
+    let mut cloned = xml_events.clone();
+    add_element_if_not_exist(&mut cloned, DcmElement {
+        tag: "0010,0010".to_string(),
+        vr: "PN".to_string(),
+        name: "PatientName".to_string(),
+        body: gdt_get_patient_name(&file.object_patient),
+    });
+
+    add_element_if_not_exist(&mut cloned, DcmElement {
+        tag: "0010,0020".to_string(),
+        vr: "LO".to_string(),
+        name: "PatientID".to_string(),
+        body: file.object_patient.patient_number,
+    });
+    add_element_if_not_exist(&mut cloned, DcmElement {
+        tag: "0010,0030".to_string(),
+        vr: "DA".to_string(),
+        name: "PatientBirthDate".to_string(),
+        body: gdt_date_to_dcm(file.object_patient.patient_dob),
+    });
+    add_element_if_not_exist(&mut cloned, DcmElement {
+        tag: "0010,0040".to_string(),
+        vr: "CS".to_string(),
+        name: "PatientSex".to_string(),
+        body: gdt_gender_to_dcm(&file.object_patient.patient_gender),
+    });
+    add_element_if_not_exist(&mut cloned, DcmElement {
+        tag: "0010,1020".to_string(),
+        vr: "DS".to_string(),
+        name: "PatientSize".to_string(),
+        body: gdt_get_patient_height_in_meters(&file.object_basic_diagnostics).unwrap_or("".to_string()),
+    });
+    add_element_if_not_exist(&mut cloned, DcmElement {
+        tag: "0010,1030".to_string(),
+        vr: "DS".to_string(),
+        name: "PatientWeight".to_string(),
+        body: file.object_basic_diagnostics.patient_weight,
+    });
+    return xml_events_to_file(cloned);
 }
 
 fn gdt_date_to_dcm(str: String) -> String {
--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,50 @@ use clap::Parser;
 use std::ffi::OsStr;
 use std::fs::{read_dir};
 use std::io::{Write};
-use std::path::{PathBuf};
+use std::path::{Path, PathBuf};
 use std::process::Command;
-use tempfile::NamedTempFile;
 
 pub mod gdt;
 pub mod dcm_xml;
 
 use crate::gdt::{ parse_file };
-use crate::dcm_xml::{ file_to_xml };
+use crate::dcm_xml::{ file_to_xml, parse_dcm_xml, default_dcm_xml };
 
 /// Convert a gdt file and an image folder to a dicom file
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    gdt_file: String,
+    gdt_file: PathBuf,
 
     #[arg(short, long)]
-    jpegs: String,
+    dicom_xml: Option<PathBuf>,
 
     #[arg(short, long)]
-    output: String,
+    jpegs: PathBuf,
+
+    #[arg(short, long)]
+    output: PathBuf,
 }
 
 fn main() -> Result<(), std::io::Error> {
     let args = Args::parse();
-    let jpegs = list_jpeg_files(args.jpegs)?;
+    let jpegs = list_jpeg_files(&args.jpegs)?;
     println!("Found Jpeg files: \n{}", jpegs.iter().map(|s| s.as_path().display().to_string()).collect::<Vec<String>>().join("\n"));
+
+    let dicom_xml_path = match args.dicom_xml {
+        Some(p) => Some(PathBuf::from(p)),
+        None => find_xml_path(&args.jpegs)?,
+    };
+    println!("Dicom XML file path: \n{}", dicom_xml_path.clone().and_then(|p| p.into_os_string().into_string().ok()).unwrap_or("None".to_string()));
+
+    let xml_events = match dicom_xml_path {
+        Some(p)=> parse_dcm_xml(&p).expect("Expecting a good xml file."),
+        _=> default_dcm_xml(),
+    };
+
     let gdt_file = parse_file(args.gdt_file).unwrap();
-    let xml = file_to_xml(gdt_file);
-    let temp_file = NamedTempFile::new()?;
-    std::fs::write(&temp_file, xml)?;
+    let temp_file = file_to_xml(gdt_file, &xml_events).unwrap();
 
     let mut command_args = vec![OsStr::new("-nsc"), OsStr::new("-dx"), temp_file.path().as_os_str()];
     command_args.extend(jpegs.iter().map(|x| OsStr::new(x)));
@@ -51,7 +63,7 @@ fn main() -> Result<(), std::io::Error> {
     return Ok(())
 }
 
-fn list_jpeg_files(dir_name: String) -> Result<Vec<PathBuf>, std::io::Error> {
+fn list_jpeg_files(dir_name: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
     let mut entries = read_dir(dir_name)?
         .filter_map(|res| res.ok().map(|e| e.path()))
         .filter(|path|
@@ -61,4 +73,11 @@ fn list_jpeg_files(dir_name: String) -> Result<Vec<PathBuf>, std::io::Error> {
         .collect::<Vec<_>>();
     entries.sort();
     return Ok(entries);
+}
+
+fn find_xml_path(dir_name: &Path) -> Result<Option<PathBuf>, std::io::Error> {
+    let xml_file = read_dir(dir_name)?
+        .filter_map(|res| res.ok())
+        .find(|dir| dir.path().extension().and_then(|s| s.to_str()) == Some("xml"));
+    return Ok(xml_file.map(|x| x.path()));
 }
