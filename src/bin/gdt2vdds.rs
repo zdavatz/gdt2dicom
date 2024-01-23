d2a8d9df0b7b1b90fe6ee44cd7c98cb55d2346b1
b123400 <i@b123400.net>
2024-01-23 14:38:25 +0900

    Set default level to info

--- src/bin/gdt2vdds.rs
+++ src/bin/gdt2vdds.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use env_logger::Env;
 use ini::Ini;
 use log::{debug, error, info};
 
@@ -27,7 +28,7 @@ struct Args {
 static PVS_NAME: &str = "gdt2dicom_PVS";
 
 fn main() -> Result<(), std::io::Error> {
-    env_logger::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let args = Args::parse();
 
     let vdds_mmi_path = &args.vdds_mmi.unwrap_or_else(vdds::default_vdds_mmi_folder);
