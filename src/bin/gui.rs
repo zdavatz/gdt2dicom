2d6af18fd30e5ac5b5017f84ba8f763c44baf6da
b123400 <i@b123400.net>
2024-08-19 23:08:20 +0900

    Basic layout with buttons to choose path and run button

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -4,7 +4,9 @@ use gdt2dicom::dcm_xml::{default_dcm_xml, file_to_xml, parse_dcm_xml, DcmTransfe
 use gtk::gio::prelude::FileExt;
 use gtk::gio::ListStore;
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button, FileDialog, FileFilter};
+use gtk::{
+    glib, Application, ApplicationWindow, Button, Entry, FileDialog, FileFilter, Grid, Label,
+};
 use std::path::{Path, PathBuf};
 
 fn main() -> glib::ExitCode {
@@ -20,27 +22,36 @@ fn main() -> glib::ExitCode {
             .default_height(70)
             .build();
 
-        let button = Button::builder()
-            .label("Press me!")
+        let input_file_label = Label::new(Some("Input File"));
+        let input_entry = Entry::new();
+        let output_file_label = Label::new(Some("Output File"));
+        let output_entry = Entry::new();
+
+        let grid_layout = Grid::builder()
+            .column_spacing(12)
+            .row_spacing(12)
             .margin_top(12)
             .margin_bottom(12)
             .margin_start(12)
             .margin_end(12)
             .build();
+        grid_layout.attach(&input_file_label, 0, 0, 1, 1);
+        grid_layout.attach(&input_entry, 1, 0, 2, 1);
+        grid_layout.attach(&output_file_label, 0, 1, 1, 1);
+        grid_layout.attach(&output_entry, 1, 1, 2, 1);
+
+        let input_button = Button::builder().label("Choose GDT file...").build();
 
         let w2 = window.clone();
-        button.connect_clicked(move |_| {
-            eprintln!("Clicked!");
-
+        let input_entry2 = input_entry.clone();
+        input_button.connect_clicked(move |_| {
+            let input_entry3 = input_entry2.clone();
             let ff = FileFilter::new();
             ff.add_suffix("gdt");
 
             let filters = ListStore::new::<FileFilter>();
             filters.append(&ff);
-
             let dialog = FileDialog::builder().filters(&filters).build();
-
-            let w3 = w2.clone();
             dialog.open(
                 Some(&w2),
                 None::<gtk::gio::Cancellable>.as_ref(),
@@ -50,27 +61,10 @@ fn main() -> glib::ExitCode {
                             println!("err {:?}", err);
                         }
                         Ok(file) => {
-                            if let Some(inputPath) = file.path() {
-                                let saveFF = FileFilter::new();
-                                saveFF.add_suffix("wl");
-                                let saveFilters = ListStore::new::<FileFilter>();
-                                saveFilters.append(&saveFF);
-                                let saveDialog =
-                                    FileDialog::builder().filters(&saveFilters).build();
-                                saveDialog.save(
-                                    Some(&w3),
-                                    None::<gtk::gio::Cancellable>.as_ref(),
-                                    move |result| match result {
-                                        Err(err) => {
-                                            println!("err {:?}", err);
-                                        }
-                                        Ok(file) => {
-                                            if let Some(outputPath) = file.path() {
-                                                convertGDTFile(&inputPath, &outputPath);
-                                            }
-                                        }
-                                    },
-                                )
+                            if let Some(input_path) = file.path() {
+                                if let Some(p) = input_path.to_str() {
+                                    input_entry3.buffer().set_text(p);
+                                }
                             }
                         }
                     }
@@ -78,21 +72,62 @@ fn main() -> glib::ExitCode {
                 },
             );
         });
-        window.set_child(Some(&button));
+        grid_layout.attach(&input_button, 3, 0, 1, 1);
 
+        let output_button = Button::builder().label("Choose WL path...").build();
+
+        let w3 = window.clone();
+        let output_entry2 = output_entry.clone();
+        output_button.connect_clicked(move |_| {
+            let output_entry3 = output_entry2.clone();
+            let ff = FileFilter::new();
+            ff.add_suffix("wl");
+
+            let filters = ListStore::new::<FileFilter>();
+            filters.append(&ff);
+            let dialog = FileDialog::builder().filters(&filters).build();
+            dialog.save(
+                Some(&w3),
+                None::<gtk::gio::Cancellable>.as_ref(),
+                move |result| {
+                    match result {
+                        Err(err) => {
+                            println!("err {:?}", err);
+                        }
+                        Ok(file) => {
+                            if let Some(input_path) = file.path() {
+                                if let Some(p) = input_path.to_str() {
+                                    output_entry3.buffer().set_text(p);
+                                }
+                            }
+                        }
+                    }
+                    eprintln!("Back from open");
+                },
+            );
+        });
+        grid_layout.attach(&output_button, 3, 1, 1, 1);
+
+        let run_button = Button::builder().label("Run").build();
+        run_button.connect_clicked(move |_| {
+            // convert_gdt_file(&input_path, &output_path);
+        });
+        grid_layout.attach(&run_button, 3, 2, 1, 1);
+
+        window.set_child(Some(&grid_layout));
         window.present();
     });
 
-    application.run()
+    return application.run();
 }
 
-fn convertGDTFile(inputPath: &Path, outputPath: &PathBuf) {
-    let gdt_file = gdt2dicom::gdt::parse_file(inputPath).unwrap();
+fn convert_gdt_file(input_path: &Path, output_path: &PathBuf) {
+    let gdt_file = gdt2dicom::gdt::parse_file(input_path).unwrap();
     let dicom_xml_path: Option<PathBuf> = None;
     let xml_events = match dicom_xml_path {
         Some(p) => parse_dcm_xml(&p).expect("Expecting a good xml file."),
         _ => default_dcm_xml(DcmTransferType::LittleEndianExplicit),
     };
     let temp_file = file_to_xml(gdt_file, &xml_events).unwrap();
-    dcm_xml_to_worklist(&temp_file.path(), outputPath);
+    dcm_xml_to_worklist(&temp_file.path(), output_path);
 }
