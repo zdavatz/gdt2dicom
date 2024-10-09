762571c584bb52b3bcd67ff8ba4c7626d181568d
b123400 <i@b123400.net>
2024-10-09 16:42:59 +0900

    Increase number of threads

--- src/bin/gdt2dicom-gui.rs
+++ src/bin/gdt2dicom-gui.rs
@@ -923,5 +923,11 @@ fn read_saved_states() -> Result<StateFile, std::io::Error> {
 
 fn runtime() -> &'static Runtime {
     static RUNTIME: OnceLock<Runtime> = OnceLock::new();
-    RUNTIME.get_or_init(|| Runtime::new().expect("Setting up tokio runtime needs to succeed."))
+    RUNTIME.get_or_init(|| {
+        tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(50)
+            .thread_name("gdt2dicom")
+            .build()
+            .expect("Setting up tokio runtime needs to succeed.")
+    })
 }
