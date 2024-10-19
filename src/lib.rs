d292e0c51351faba3ac814e3cdb3d44b7f795345
b123400 <i@b123400.net>
2024-10-19 21:20:34 +0900

    Only include gui for gui binary

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ pub mod dcm_xml;
 pub mod dicom_jpeg_extraction;
 pub mod error;
 pub mod gdt;
-pub mod gui;
 pub mod opp_xml;
 pub mod vdds;
 pub mod worklist_conversion;
+
+#[cfg(feature = "gui")]
+pub mod gui;
